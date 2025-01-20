e821b4441a4860e84e1505a4268e1a79d5e0a6fd
Juan David Gomez Baena <juan.gomez@xpertgroup.co>
2025-01-20 15:25:29 -0500

    .

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -13,7 +13,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatInputModule } from '@angular/material/input'; // Asegúrate de importar MatInputModule
+import { MatInputModule } from '@angular/material/input';
 import { MatNativeDateModule, provideNativeDateAdapter } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -124,7 +124,6 @@ export class FormComponent implements OnInit {
   createOrder() {
   }
 
-  // Método para cerrar o resetear el formulario
   resetForm(): void {
     this.orderForm.reset();
     this.dialogRef.close();
